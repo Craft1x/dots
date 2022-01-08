b85840e6511053e84f9e35d2b08c5529634b1332
Craft1x <shamin.518555@gmail.com>
2022-01-08 23:45:00 +0300

    added a shortcut and a phone battery charge indicator

--- polybar/.config/polybar/scripts/phone-charge.sh
+++ polybar/.config/polybar/scripts/phone-charge.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 DATA=`qdbus org.kde.kdeconnect /modules/kdeconnect/devices/8f54298fa462dd30/battery org.kde.kdeconnect.device.battery.charge` || ( echo ""; return 0; )
+ISCHARGING=`qdbus org.kde.kdeconnect /modules/kdeconnect/devices/8f54298fa462dd30/battery org.kde.kdeconnect.device.battery.isCharging`
 
-ICON=""
 COLOR="#60BAEC"
 SEPCOLOR="#3F5360"
+ICON=""
 
 if [ $DATA -ge 95 ]; then
 	ICON=""
@@ -15,6 +16,10 @@ elif [ $DATA -ge 40 ]; then
 	ICON=""
 elif [ $DATA -ge 20 ]; then 
 	ICON=""
+fi
+
+if [ $ISCHARGING = true ]; then
+	COLOR="#FFCB6B"
 fi
 
 
