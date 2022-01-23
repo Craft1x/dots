6e274154546ff5dae20e9b6487d96e283135247b
craftix <shamin.518555@gmail.com>
2022-01-23 20:36:18 +0300

    added nvidia display to polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -15,6 +15,7 @@ MONITORS=`xrandr --listmonitors`
 echo $MONITORS | grep DisplayPort-0 && polybar -q main -c "$DIR"/config.ini &
 echo $MONITORS | grep HDMI-A-0 && polybar -q main2 -c "$DIR"/config.ini &
 echo $MONITORS | grep eDP-1 && polybar -q main3 -c "$DIR"/config.ini &
+echo $MONITORS | grep eDP-1-1 && polybar -q main4 -c "$DIR"/config.ini &
 
 # IPC settings and test
 ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-main
