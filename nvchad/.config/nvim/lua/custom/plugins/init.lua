79a4a7a6e5ac4fc5fa2382eb5bb10738997da671
craftix <shamin.518555@gmail.com>
2022-03-05 14:02:24 +0300

    Added VNC screen for polybar, shortened the titlebar

---
+++ nvchad/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,6 @@
+-- /lua/custom/plugins/init.lua
+-- return {
+--    { "elkowar/yuck.vim", ft = "yuck" },
+--    { "ellisonleao/glow.nvim", cmd = "Glow" },
+-- }
+-- just an example!
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -16,6 +16,7 @@ echo $MONITORS | grep DisplayPort-0 && polybar -q main -c "$DIR"/config.ini &
 echo $MONITORS | grep HDMI-A-0 && polybar -q main2 -c "$DIR"/config.ini &
 echo $MONITORS | grep eDP-1 && polybar -q main3 -c "$DIR"/config.ini &
 echo $MONITORS | grep eDP-1-1 && polybar -q main4 -c "$DIR"/config.ini &
+echo $MONITORS | grep VNC-0 && polybar -q mainVNC -c "$DIR"/config.ini &
 
 # IPC settings and test
 ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-main
