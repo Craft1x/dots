05e0d11710a4fd26ac4c02407ead72a8256d1da3
Craft1x <shamin.518555@gmail.com>
2022-05-19 18:05:18 +0300

    Added power manager to rofi; added phone notifications to polybar

---
+++ polybar/.config/polybar/scripts/phone-notifications.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source "$HOME/.config/polybar/scripts/source-colors.sh" 
+
+DATA=`qdbus org.kde.kdeconnect /modules/kdeconnect/devices/8f54298fa462dd30/notifications org.freedesktop.DBus.Introspectable.Introspect | grep "<node name=" | wc -l` || ( echo ""; return 0; )
+
+
+COLOR="${green}"
+ICON="∙"
+
+[[ $DATA == "0" ]] && echo "" || echo "%{F$COLOR}$ICON%{F-}"
---
+++ rofi/.config/rofi/power.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+theme="style"
+dir="$HOME/.config/rofi"
+
+strings=("Lock Screen" "Shut Down / Power Off" "Reboot / Restart" "Logout")
+icons=("lock.png" "power-off.png" "reboot.png" "logout.png")
+commands=("cinnamon-screensaver-command --lock" "poweroff" "reboot" "i3-msg exit")
+
+prompt=""
+length=${#strings[@]}
+
+for (( j=0; j<${length}; j++ ));
+do
+  prompt="$prompt${strings[$j]}\x00icon\x1f~/.config/rofi/icons/${icons[$j]}\n";
+done
+
+result=`printf "$prompt" | rofi -no-lazy-grab -sort -sorting-method fzf -dmenu -p "" -i -theme $dir/"$theme"` 
+
+for (( j=0; j<${length}; j++ ));
+do
+  [[ $result == ${strings[$j]} ]] && ${commands[$j]} && break
+done
