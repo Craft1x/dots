4969af7e59edfa0627c07f9fd8ef31a8ce66a55b
Craft1x <shamin.518555@gmail.com>
2022-06-26 22:38:09 +0300

    Added game mode icon to polybar

---
+++ polybar/.config/polybar/scripts/gamemode.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source "$HOME/.config/polybar/scripts/source-colors.sh" 
+
+ENABLED=`head -n 1 $HOME/.config/sxhkd/sxhkdrc | grep -q gamemode && echo 1 || echo 0`
+
+if [[ $ENABLED == "0" ]]; then
+  echo "" 
+  exit 0
+fi
+
+icon_color="${blue}"
+
+echo "$separator%{A1:$HOME/.config/sxhkd/gamemode.sh disable:} %{F$icon_color}調 %{F-}%{A}"
