f77c1ba8043f8ba03e83120e1523842886aa89e9
craftix <shamin.518555@gmail.com>
2022-07-21 14:08:15 +0300

    Added a battery monitor script

---
+++ autostart/.config/scripts/battery-monitor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+NOTIFIED=false
+SUSPENDED=false
+
+while true; do
+  sleep 5
+  DATA=`cat /sys/class/power_supply/BAT0/capacity` || ( echo ""; return 0; )
+  ISCHARGING=`cat /sys/class/power_supply/BAT0/status | grep -q Discharging && echo false || echo true`
+
+  [[ "$ISCHARGING" = true ]] && continue
+
+  if [ "$DATA" -lt 5 ]; then
+    if [[ $SUSPENDED == false ]]; then
+      SUSPENDED=true
+      logger "Critical battery threshold"
+      notify-send "Suspending" -i laptopattention
+      systemctl suspend
+    fi
+  else
+    SUSPENDED=false
+  fi
+
+  if [[ "$DATA" -lt 15 ]]; then
+    if [[ $NOTIFIED == false ]]; then
+      NOTIFIED=true
+      notify-send "Low battery" -i laptopattention
+      light -S 20
+    fi
+  else
+    NOTIFIED=false
+  fi
+
+done
