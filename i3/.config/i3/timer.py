cd47bb040e0adfb66e996a8978944fdfcdc5c985
Craft1x <shamin.518555@gmail.com>
2023-04-04 15:39:27 +0300

    Polybar improve perfomance

--- i3/.config/i3/timer.py
+++ i3/.config/i3/timer.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
-import socket,os,threading,sys,datetime
+
 from threading import Thread
+import sys,socket
 from time import sleep
-from datetime import datetime, timedelta
-import subprocess
-import dateparser
 
 timer = 0 
 TIMEOUT_MESSAGE="Time's up!"
-TIMER_SAVEFILE=os.path.expanduser('~')+"/.config/saved_timer"
-TIMEOUT_COMMAND="dunstify -r 94123 -i ~/.config/sxhkd/icons/timer.svg -u critical".split()+ ["Time's up!","middle click to snooze"] + ["--action=snooze,Snooze"]
 
 
-def unix_from(datetimeFrom):
+def unix_from(datetime,datetimeFrom):
     unix_timestamp = datetime.timestamp(datetimeFrom) * 1000
     return unix_timestamp
 
-def unix():
+def unix(datetime):
     presentDate = datetime.now()
-    return unix_from(presentDate)
-
+    return unix_from(datetime,presentDate)
 
 def send(dataStr):
     try:
@@ -35,14 +30,31 @@ def send(dataStr):
 
 class ThreadedServer(Thread):
 
+  import os
+  from datetime import datetime, timedelta
+
+
   def __init__(self):
+    global timer
     Thread.__init__(self)
+
+
+    self.savefile=self.os.environ['HOME']+"/.config/saved_timer"
     self.s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     self.s.bind("/tmp/timersocket")
     self.s.listen(1)
 
+    if self.os.path.isfile(self.savefile):
+        f = open(self.savefile, 'r')
+        input = float(f.read())
+        timer = input
+        print('read saved',input)
+        f.close()
+        
+
   def run(self):
     global timer
+
     while True:
         conn, addr = self.s.accept()
         data = conn.recv(1024)
@@ -52,9 +64,9 @@ class ThreadedServer(Thread):
             if got[0] == 'set':
                 val = int(got[1])
                 print('set timer to', val)
-                timer = unix() + val
+                timer = unix(self.datetime) + val
 
-                f = open(TIMER_SAVEFILE, "w")
+                f = open(self.savefile, "w")
                 f.write(str(timer))
                 f.close()
 
@@ -64,17 +76,30 @@ class ThreadedServer(Thread):
                 timer = 0
 
                 try:
-                    os.remove(TIMER_SAVEFILE)
+                    self.os.remove(self.savefile)
                 except OSError:
                     pass
                 conn.send(b'ok')
             if got[0] == 'get':
                 if timer == 0:
                     conn.send(b'off')
-                elif timer - unix() < 0:
+                elif timer - unix(self.datetime) < 0:
                     conn.send(TIMEOUT_MESSAGE.encode())
                 else:
-                    conn.send(str(timer - unix()).encode())
+                    conn.send(str(timer - unix(self.datetime)).encode())
+            if got[0] == 'getpretty':
+                if timer == 0:
+                    conn.send(b'off')
+                elif timer - unix(self.datetime) < 0:
+                    conn.send(TIMEOUT_MESSAGE.encode())
+                else:
+                    result =str(timer - unix(self.datetime))
+                    if result.replace('.','',1).isdigit():
+                        result=self.timedelta(milliseconds=(int(float(result)/1000)*1000 + 1000))
+                        result=str(result).lstrip("00:").lstrip("0:").lstrip("0")
+                        if len(result) ==0:
+                            result="0"
+                    result = conn.send(result.encode())
         conn.close()
 
 
@@ -87,12 +112,7 @@ def main(argv):
         print(result)
         return
     if (len(argv) == 1 and argv[0] == 'getpretty'):
-        result = send('get')
-        if result.replace('.','',1).isdigit():
-            result=timedelta(milliseconds=(int(float(result)/1000)*1000 + 1000))
-            result=str(result).lstrip("00:").lstrip("0:").lstrip("0")
-            if len(result) ==0:
-                result="0"
+        result = send('getpretty')
         print(result)
         return
     if (len(argv) == 2 and argv[0] == 'set'):
@@ -103,13 +123,17 @@ def main(argv):
     if (len(argv) == 2 and argv[0] == 'sethuman'):
         input = argv[1].lstrip("in")
 
+        from datetime import datetime
+
         if input.lower() == "reset":
             send('reset')
             return
 
+        import dateparser
+
         parsed = dateparser.parse("in "+input)
         if parsed != None:
-            timeSet = int(unix_from(parsed) - unix())
+            timeSet = int(unix_from(datetime,parsed) - unix(datetime))
             print("setting to", timeSet)
             send('set '+ str(timeSet))
         return
@@ -119,25 +143,22 @@ def main(argv):
         return
 
 
+    from datetime import datetime
+    import subprocess
+    import os
+
     try:
         os.remove("/tmp/timersocket")
     except OSError:
         pass
 
-    if os.path.isfile(TIMER_SAVEFILE):
-        f = open(TIMER_SAVEFILE, 'r')
-        input = float(f.read())
-        timer = input
-        print('read saved',input)
-        f.close()
-        
-
     clientThread = ThreadedServer()
     clientThread.start()
     
     while 1:
-        if timer > 0 and timer - unix() <=0:
-            process = subprocess.Popen(TIMEOUT_COMMAND, stdout=subprocess.PIPE)
+        if timer > 0 and timer - unix(datetime) <=0:
+            timeoutCommand="dunstify -r 94123 -i ~/.config/sxhkd/icons/timer.svg -u critical".split()+ ["Time's up!","middle click to snooze"] + ["--action=snooze,Snooze"]
+            process = subprocess.Popen(timeoutCommand, stdout=subprocess.PIPE)
             output, error = process.communicate()
             result = output.decode("utf-8")
             print(result)
