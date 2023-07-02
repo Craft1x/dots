a96861cf282feacc6ba732ff67122cb9787b6a46
Craft1x <shamin.518555@gmail.com>
2023-07-02 21:10:09 +0300

    mpv fix

--- mpv/.config/mpv/scripts/locatefile.lua
+++ mpv/.config/mpv/scripts/locatefile.lua
@@ -12,37 +12,11 @@ end
 
 -- for ubuntu
 url_browser_linux_cmd = "xdg-open \"$url\""
-file_browser_linux_cmd = "dbus-send --print-reply --dest=org.freedesktop.FileManager1 /org/freedesktop/FileManager1 org.freedesktop.FileManager1.ShowItems array:string:\"file:$path\" string:\"\""
--- for macos
-url_browser_macos_cmd = "open \"$url\""
-file_browser_macos_cmd = "osascript -e 'tell application \"Finder\"' -e 'set frontmost to true' -e 'reveal (POSIX file \"$path\")' -e 'end tell'"
--- for windows
-url_browser_windows_cmd = "explorer \"$url\""
-file_browser_windows_cmd = "explorer /select,\"$path\""
+file_browser_linux_cmd = "nemo \"$path\""
 
 --// check if it's a url/stream
 function is_url(path)
   if path ~= nil and string.sub(path,1,4) == "http" then
-    return true
-  else
-    return false
-  end
-end
-
---// check if macos
-function is_macos()
-  local homedir = os.getenv("HOME")
-  if homedir ~= nil and string.sub(homedir,1,6) == "/Users" then
-    return true
-  else
-    return false
-  end
-end
-
---// check if windows
-function is_windows()
-  local windir = os.getenv("windir")
-  if windir~=nil then
     return true
   else
     return false
@@ -65,34 +39,17 @@ mp.register_script_message("locate-current-file", function()
     local cmd = ""
     if is_url(path) then
       msg.debug("Url detected '" .. path .. "', your OS web browser will be launched.")
-      if is_windows() then
-        msg.debug("Windows detected.")
-        cmd = url_browser_windows_cmd
-      elseif is_macos() then
-        msg.debug("macOS detected.")
-        cmd = url_browser_macos_cmd
-      else
-        msg.debug("Linux detected.")
-        cmd = url_browser_linux_cmd
-      end
+      msg.debug("Linux detected.")
+      cmd = url_browser_linux_cmd
       cmd = cmd:gsub("$url", path)
     else
       msg.debug("File detected '" .. path .. "', your OS file browser will be launched.")
-      if is_windows() then
-        msg.debug("Windows detected.")
-        cmd = file_browser_windows_cmd
-        path = path:gsub("/", "\\")
-      elseif is_macos() then
-        msg.debug("macOS detected.")
-        cmd = file_browser_macos_cmd
-      else
-        msg.debug("Linux detected.")
-        cmd = file_browser_linux_cmd
-      end
+      msg.debug("Linux detected.")
+      cmd = file_browser_linux_cmd
       cmd = cmd:gsub("$path", path)
     end
     msg.debug("Command to be executed: '" .. cmd .. "'")
-    mp.osd_message('Browse \n' .. path)
+    -- mp.osd_message('Browse \n' .. path)
     os.execute(cmd)
   else
     msg.debug("'path' property was empty, no media has been loaded.")
